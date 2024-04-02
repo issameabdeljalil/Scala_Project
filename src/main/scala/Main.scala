e8da3c2ba6a18a23a3d0e1ce319dd86158b3f0a5
eliamosef <118312033+eliamosef@users.noreply.github.com>
2024-04-02 22:39:06 +0200

    Refactoring (#10)
    * Refactoring (#8)
    
    * Refactoring App
    
    * App refactoring
    
    * Refactoring (#9)
    
    * Refactoring App
    
    * App refactoring
    
    * Refactoring
    
    * Renaming project

--- src/main/scala/Main.scala
+++
@@ -1,7 +0,0 @@
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.SparkContext
-object Main extends App {
-  println("Hello, World!")
-  val sparkSession = SparkSession.builder.master("local[1]").enableHiveSupport().getOrCreate()
-  print(sparkSession.sql("SELECT 'A'").show())
-}
